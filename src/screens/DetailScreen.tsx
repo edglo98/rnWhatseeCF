cee332742b5ff5b97071f985e518f8bc3fd87b75
Edgar Lopez <edgarlopezmgs@live.com>
2021-11-24 22:34:53 -0600

    feat: getter movie details from url params

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,10 +1,16 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {View, Text} from 'react-native';
+import {RootStackParams} from '../navigations/MainNavigation';
 
-const DetailScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'moviedetails'> {}
+
+const DetailScreen = ({route}: Props) => {
+  const movie = route.params;
+  console.log(movie);
   return (
     <View>
-      <Text>Detail screen</Text>
+      <Text>{movie.title}</Text>
     </View>
   );
 };
