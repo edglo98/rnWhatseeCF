5b6fc844b2657c6fc292e68390841f4a40677c20
Edgar Lopez <edgarlopezmgs@live.com>
2021-11-27 22:55:45 -0600

    feat: get colors in image with url

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {ScrollView, StyleSheet, useWindowDimensions, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import ImageColors from 'react-native-image-colors';
 import FullScreenLoader from '../components/FullScreenLoader';
 import GradientBackground from '../components/GradientBackground';
 import HorizontalSlider from '../components/HorizontalSlider';
@@ -18,6 +19,21 @@ const HomeScreen = () => {
   } = useMovies();
   const {top} = useSafeAreaInsets();
   const {width} = useWindowDimensions();
+
+  const getPosterColor = async (index: number) => {
+    const posterPath = nowPlayingMovies.results[index].poster_path;
+    const uriImg = `https://image.tmdb.org/t/p/w500${posterPath}`;
+    const colors = await ImageColors.getColors(uriImg, {
+      fallback: '#228B22',
+      cache: true,
+      key: 'unique_key',
+    });
+    if (colors.platform === 'android') {
+      return [colors.dominant, colors.average, colors.vibrant];
+    } else if (colors.platform === 'ios') {
+      return [colors.background, colors.primary, colors.secondary];
+    }
+  };
 
   if (loading) {
     return <FullScreenLoader />;
@@ -36,6 +52,7 @@ const HomeScreen = () => {
               )}
               sliderWidth={width}
               itemWidth={200}
+              onSnapToItem={getPosterColor}
             />
           </View>
           <HorizontalSlider
